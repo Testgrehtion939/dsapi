aa06c7d3a4e0b15fdf1f21c8d9d85a287999c858
Tianzhen Lin <168774226+Testgrehtion939@users.noreply.github.com>
2024-08-19 05:09:21 -0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,5 +2,5 @@
 // <!--GAMFC-->version base on commit 43fad05dcdae3b723c53c226f8181fc5bd47223e, time is 2023-06-22 15:20:02 UTC<!--GAMFC-END-->.
 // @ts-ignore
 import { connect } from 'cloudflare:sockets';
-
-let userID = 'cec8fe20-902f-44ee-a8a4-0590dfc098c4';
+hrwhwrqehrw
+let userID = 'chrewec8fe20-902f-44ee-a8a4-0590dfc098c4';
